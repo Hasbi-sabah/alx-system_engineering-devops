03a9c63de42915f59521f2650051061de25546a6
H. Sabah <sabahhasbi00@gmail.com>
2023-09-23 11:48:34 +0000

    Code fix: all in one line

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,20 +1,8 @@
 file_line{'Set alias name':
   path => '/etc/ssh/ssh_config',
-  line => 'Host 54.210.89.176',
-}
-file_line{'Set host name':
-  path => '/etc/ssh/ssh_config',
-  line => '    HostName 54.210.89.176',
-}
-file_line{'Set user name':
-  path => '/etc/ssh/ssh_config',
-  line => '    User ubuntu',
-}
-file_line{'Set identity file':
-  path => '/etc/ssh/ssh_config',
-  line => '    IdentityFile ~/.ssh/school',
-}
-file_line{'Turn off passwd auth':
-  path => '/etc/ssh/ssh_config',
-  line => '    PasswordAuthentication no',
+  line => 'Host 54.210.89.176
+    HostName 54.210.89.176
+    User ubuntu
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no',
 }
