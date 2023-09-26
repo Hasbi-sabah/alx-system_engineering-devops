ba577eecd9feab3ea4659e16500c3bb303424bd4
H. Sabah <sabahhasbi00@gmail.com>
2023-09-26 16:37:28 +0000

    Code fix: puppet lint fixed

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,20 +5,20 @@ exec { 'apt-update':
   refreshonly => true,
 }
 exec { 'apt-get':
-  command     => 'apt-get -y install nginx',
-  path        => '/usr/bin:/bin',
+  command => 'apt-get -y install nginx',
+  path    => '/usr/bin:/bin',
   require => Exec['apt-update'],
-} ->
+}
 
-file { '/var/www/html/index.nginx-debian.html':
+-> file { '/var/www/html/index.nginx-debian.html':
   content => 'Hello World!',
 }
 
 file_line { 'add_redirect':
-  path  => '/etc/nginx/sites-enabled/default',
-  after => 'server_name _;',
-  line  => '    location /redirect_me {return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;}',
-  notify  => Exec['restart_nginx'],
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'server_name _;',
+  line   => '    location /redirect_me {return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;}',
+  notify => Exec['restart_nginx'],
 }
 
 exec { 'restart_nginx':
