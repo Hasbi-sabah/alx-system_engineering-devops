65083481ee4192f543a48660b23937f24913adda
H. Sabah <sabahhasbi00@gmail.com>
2023-09-26 16:35:55 +0000

    Code fix: reworked the whole code

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,8 +4,9 @@ exec { 'apt-update':
   path        => '/usr/bin:/bin',
   refreshonly => true,
 }
-package { 'nginx':
-  ensure => 'installed',
+exec { 'apt-get':
+  command     => 'apt-get -y install nginx',
+  path        => '/usr/bin:/bin',
   require => Exec['apt-update'],
 } ->
 
@@ -13,24 +14,15 @@ file { '/var/www/html/index.nginx-debian.html':
   content => 'Hello World!',
 }
 
-file { '/etc/nginx/sites-enabled/default':
-  ensure => file,
-}
-
-file_line { 'add redirect':
+file_line { 'add_redirect':
   path  => '/etc/nginx/sites-enabled/default',
-  match => 'server_name _;',
-  line  => '     location /redirect_me {return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;}',
-}
-
-service { 'nginx':
-  ensure    => 'running',
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-enabled/default'],
-  notify    => Exec['restart_nginx'],
+  after => 'server_name _;',
+  line  => '    location /redirect_me {return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;}',
+  notify  => Exec['restart_nginx'],
 }
 
 exec { 'restart_nginx':
-  command     => '/usr/bin/sudo /usr/sbin/service nginx restart',
+  command     => '/usr/sbin/service nginx restart',
   refreshonly => true,
+  subscribe   => File_line['add_redirect'],
 }
