6f1e08ea2334efbc5459c04fb3a9f282fe4ba586
H. Sabah <sabahhasbi00@gmail.com>
2023-10-02 19:10:44 +0000

    Code fix: all exec

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,24 +1,20 @@
 # instal and configure nginx
-exec { 'apt-update':
-  command     => 'apt-get -y update',
-  path        => '/usr/bin:/bin',
-  refreshonly => true,
-}
-exec { 'apt-get':
-  command => 'apt-get -y install nginx',
-  path    => '/usr/bin:/bin',
-  require => Exec['apt-update'],
+exec { 'update':
+  command     => 'sudo apt-get -y update',
+  provider => shell,
 }
 
--> file_line { 'add_header':
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => "^\tlocation / {",
-  line   => "\t\tadd_header X-Served-By \"${::hostname}\";",
-  notify => Exec['restart_nginx'],
+exec { 'install':
+  command => 'sudo apt-get -y install nginx',
+  provider => shell,
 }
 
-exec { 'restart_nginx':
-  command     => '/usr/sbin/service nginx restart',
-  refreshonly => true,
-  subscribe   => File_line['add_redirect'],
+exec { 'replace':
+  command => 'sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-enabled/default',
+  provider => shell,
+}
+
+exec { 'restart':
+  command => 'sudo service nginx restart',
+  provider => shell,
 }
