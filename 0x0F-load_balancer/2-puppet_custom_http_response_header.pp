22ef528374cabf05fcde62d31baf57b0f1968915
H. Sabah <sabahhasbi00@gmail.com>
2023-10-02 11:49:48 +0000

    Files added: task 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,41 @@
+# instal and configure nginx
+exec { 'apt-update':
+  command     => 'apt-get -y update',
+  path        => '/usr/bin:/bin',
+  refreshonly => true,
+}
+exec { 'apt-get':
+  command => 'apt-get -y install nginx',
+  path    => '/usr/bin:/bin',
+  require => Exec['apt-update'],
+}
+
+-> file { '/var/www/html/index.nginx-debian.html':
+  content => 'Hello World!',
+}
+
+file_line { 'add_redirect':
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'server_name _;',
+  line   => '    location /redirect_me {return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;}',
+  notify => Exec['restart_nginx'],
+}
+
+$hostname = exec {'hostname':
+  command => 'hostname'
+  path    => '/usr/bin:/bin',
+  logoutput   => true,
+  refreshonly => true,
+}
+
+file_line { 'add_header':
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'location / {'
+  line => '     add_header X-Served-By \"${hostname}\";'
+}
+
+exec { 'restart_nginx':
+  command     => '/usr/sbin/service nginx restart',
+  refreshonly => true,
+  subscribe   => File_line['add_redirect'],
+}
