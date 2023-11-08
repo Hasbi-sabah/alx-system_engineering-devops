a5ff4bfd5d5ddd669379e640066ee790408bf2fc
H. Sabah <sabahhasbi00@gmail.com>
2023-11-08 09:58:59 +0000

    Code fix: using exec instead of file line

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,4 @@
-file_line { 'fix_typo':
-        path  => '/var/www/html/wp-settings.php',
-        line  => 'php',
-        match => 'phpp',
+exec { 'fix_typo':
+        command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+        provider => 'shell'
 }
