71b1050cae85cec33adaf795e54208d3451140c5
H. Sabah <sabahhasbi00@gmail.com>
2023-11-14 15:23:43 +0000

    Files added: task 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# increase max open files for holberton user
+file_line { 'replace_hard_limit':
+  path => '/etc/security/limits.conf',
+  line => 'holberton hard nofile 5000',
+  match => '^holberton hard',
+}
+file_line { 'replace_soft_limit':
+  path => '/etc/security/limits.conf',
+  line => 'holberton soft nofile 4000',
+  match => '^holberton soft',
+}
