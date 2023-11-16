49f213407661b2c393728efb899bec023bc54e28
H. Sabah <sabahhasbi00@gmail.com>
2023-11-16 09:24:38 +0000

    Code fix: sed instead of file_line

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,9 @@
 # increase max open files for holberton user
-file_line { 'replace_hard_limit':
-  path  => '/etc/security/limits.conf',
-  line  => 'holberton hard nofile 5000',
-  match => '^holberton hard',
+exec { 'replace_hard_limit':
+  command  => 'sed "s/holberton hard nofile 5/holberton hard nofile 5000/" /etc/security/limits.conf',
+  provider => 'shell'
 }
-file_line { 'replace_soft_limit':
-  path  => '/etc/security/limits.conf',
-  line  => 'holberton soft nofile 4000',
-  match => '^holberton soft',
+exec { 'replace_soft_limit':
+  command  => 'sed "s/holberton soft nofile 4/holberton soft nofile 4000/" /etc/security/limits.conf',
+  provider => 'shell'
 }
