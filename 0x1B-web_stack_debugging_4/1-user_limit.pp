40a71a2d07c4dc5740f9b7bf12911987d4474763
H. Sabah <sabahhasbi00@gmail.com>
2023-11-16 09:28:26 +0000

    Code fix: missing -i

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,9 @@
 # increase max open files for holberton user
 exec { 'replace_hard_limit':
-  command  => 'sed "s/holberton hard nofile 5/holberton hard nofile 5000/" /etc/security/limits.conf',
+  command  => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 5000/" /etc/security/limits.conf',
   provider => 'shell'
 }
 exec { 'replace_soft_limit':
-  command  => 'sed "s/holberton soft nofile 4/holberton soft nofile 4000/" /etc/security/limits.conf',
+  command  => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 4000/" /etc/security/limits.conf',
   provider => 'shell'
 }
