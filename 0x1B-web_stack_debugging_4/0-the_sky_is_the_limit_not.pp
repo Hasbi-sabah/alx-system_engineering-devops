57a9a044f9766969f9d28c83c5cb6dcac087dae7
H. Sabah <sabahhasbi00@gmail.com>
2023-11-16 09:14:55 +0000

    Code fix: exec instead of file_line

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,7 @@
 # increase max open files
-file_line { 'replace_line':
-  path  => '/etc/default/nginx',
-  line  => 'ULIMIT="-n 10000"',
-  match => '^ULIMIT',
+exec { 'replace_line':
+  command  => 'sed -i "s|ULIMIT=\"-n 15\"|ULIMIT=\"-n 10000\"|" /etc/default/nginx',
+  provider => 'shell',
 }
 -> exec { 'restart_nginx':
   command  => 'service nginx restart',
