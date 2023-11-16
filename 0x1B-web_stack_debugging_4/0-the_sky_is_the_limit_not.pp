2e4fa868d00cbd0edae9b6cf9255d5df3cc1711f
H. Sabah <sabahhasbi00@gmail.com>
2023-11-16 08:10:15 +0000

    Code fix: puppet lint

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,10 @@
 # increase max open files
 file_line { 'replace_line':
-  path => '/etc/default/nginx',
-  line => 'ULIMIT="-n 10000"',
+  path  => '/etc/default/nginx',
+  line  => 'ULIMIT="-n 10000"',
   match => '^ULIMIT',
 }
 -> exec { 'restart_nginx':
-  command => 'service nginx restart',
+  command  => 'service nginx restart',
   provider => 'shell',
 }
